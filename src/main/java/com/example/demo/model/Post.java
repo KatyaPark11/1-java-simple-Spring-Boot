d8481aa5b316d0bfccd4a9c7851a10974ba29af3
KatyaPark11 <127592285+KatyaPark11@users.noreply.github.com>
2024-02-18 14:42:55 +0500

    BootStrap

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,12 +1,20 @@
 package com.example.demo.model;
 
+import java.util.Date;
+
 public class Post {
+    private Date creationDate;
     private String text;
     private Integer likes;
 
-    public Post(String text) {
+    public Post(String text, Date creationDate) {
+        this.creationDate = creationDate;
         this.text = text;
         this.likes = 0;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
     }
 
     public String getText() {
--- src/main/java/com/example/demo/service/PostService.java
+++ src/main/java/com/example/demo/service/PostService.java
@@ -3,15 +3,17 @@ package com.example.demo.service;
 import com.example.demo.model.Post;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Service
 public class PostService {
     public ArrayList<Post> listAllPosts() {
         ArrayList<Post> posts = new ArrayList<>();
-        posts.add(new Post("Первый постик"));
-        posts.add(new Post("Второй постик"));
-        posts.add(new Post("Третий постик"));
+        posts.add(new Post("Первый постик", new Date()));
+        posts.add(new Post("Второй постик", new Date()));
+        posts.add(new Post("Третий постик", new Date()));
         return posts;
     }
 }
